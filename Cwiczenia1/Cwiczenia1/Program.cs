2cf8558db09fc492b3289d745cfd043d0e033c57
s16503 <s16503@pjwstk.edu.pl>
2020-03-06 19:51:01 +0100

    wyszukiwanie adresów email

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,13 +1,44 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cwiczenia1
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            // raz raz
-            Console.WriteLine("Hello World!");
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(args[0]);
+
+           // Console.Write(response);
+
+   
+            Regex reg1 = new Regex(@"\b[a-z][a-z0-9_]*@[a-z0-9]*\.[a-z]{2,3}\b");
+            Regex reg2 = new Regex(@"\b[a-z][a-z0-9_]*@[a-z0-9]*\.[a-z0-9]*\.[a-z]{2,3}\b");
+
+
+        
+
+
+            foreach( Match m in reg1.Matches(response) )
+            {
+                Console.WriteLine(m.Value);
+            }
+
+            foreach (Match m in reg2.Matches(response))
+            {
+                Console.WriteLine(m.Value);
+            }
+
+            //while(reg1.IsMatch(response))
+            //{
+            //    Console.Write(reg1.Match(response));
+            //    reg1.
+            //}
+
+            
         }
     }
 }
